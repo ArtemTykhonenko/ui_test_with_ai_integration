f43418e9a95cad54794397622411ed063a6ef866
Home <artem.tykhonenko.v@gmail.com>
2024-11-15 01:38:38 +0200

    Selenide logic with API

--- src/main/java/automation/utils/Constants.java
+++ src/main/java/automation/utils/Constants.java
@@ -16,4 +16,10 @@ public class Constants {
     public static final String ERROR_USER = "error_user";
     public static final String VISUAL_USER = "visual_user";
     public static final String PASSWORD = "secret_sauce";
+
+    /**
+     * API key
+     **/
+    public static final String ChatGPTAPI = "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";
+
 }
